b78902c509fb00c0a57c16a88d5f6d88099a2df3
nhatminhdao <nhatminhdao@gmail.com>
2024-08-01 01:50:32 +0000

    models edit

--- app/models/director.rb
+++ app/models/director.rb
@@ -11,4 +11,11 @@
 #  updated_at :datetime         not null
 #
 class Director < ApplicationRecord
+  def filmograpphy
+    my_id = self.id
+
+    matching_movies = Movie.where({ :director_id => my_id })
+
+    return matching movies
+  end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,4 +13,18 @@
 #  director_id :integer
 #
 class Movie < ApplicationRecord
+  def director
+    director_id = self.director_id
+    matching_directors = Director.where({ :id => director_id })  
+    the_director = matching_directors.first
+    return the_director
+  end
+
+  def characters
+    my_id = self.id
+    matching_characters = Character.where(:id => my_id)
+    return matching_characters
+    # the_characters = matching_characters.all
+    # return the characters
+  end
 end
